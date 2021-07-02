570a4af9c37417e3ae7dd720c29c9f02238865b4
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-02 15:03:49 +0545

    middleware registration & invocation demonstration

--- server.go
+++ server.go
@@ -12,6 +12,39 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func Middleware1() gin.HandlerFunc {
+	log.Println("Demo Middleware1 Registered")
+	return func(c *gin.Context) {
+		log.Println("Demo Middleware1 Executed")
+		c.Next()
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	log.Println("CORS Middleware2 Registered")
+	return func(c *gin.Context) {
+		// CORS middleware
+		// this will allow direct usage of axios requests from next js app
+		// no more request forwarding from api/ directory a.k.a request forwarding for API (serverIP/*) through API routes (/api/*)
+		log.Println("CORS Middleware2 Executed")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
+func Middleware2() gin.HandlerFunc {
+	log.Println("Demo Middleware3 Registered")
+	return func(c *gin.Context) {
+		log.Println("Demo Middleware3 Executed")
+		c.Next()
+	}
+}
+
+
 func main() {
 	router := gin.Default()
 
@@ -20,14 +53,10 @@ func main() {
 	storageClient := infrastructures.InitStorageClient()
 	storageService := services.NewGCPStorageService(storageClient)
 
-	router.Use(func(c *gin.Context){
-		// CORS middleware
-		// this will allow direct usage of axios requests from next js app
-		// no more request forwarding from api/ directory a.k.a request forwarding for API (serverIP/*) through API routes (/api/*)
-		c.Writer.Header().Set("Access-Control-Allow-Origin","http://localhost:3001")
-		
-		c.Next()
-	})
+	router.Use(Middleware1())
+	router.Use(CORSMiddleware())
+	router.Use(Middleware2())
+
 	
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
