1144575db24dec5d372ba5f6e6d2a743fb5361bf
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-09 15:09:54 +0545

    add CORS handler

--- server.go
+++ server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/Amitmahato/golang-file-upload-server/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
 	httpRouter := gin.Default()
+	httpRouter.Use(middleware.CORSMiddleware())
 
 	httpRouter.GET("/ping",func(c *gin.Context) {
 		c.JSON(http.StatusOK,gin.H{
